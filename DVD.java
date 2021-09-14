2fc43d85c672ccccbc12b0836b97f22edd7da099
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-09-13 18:12:49 -0700

    Initial commit

---
+++ DVD.java
@@ -0,0 +1,72 @@
+public class DVD {
+
+	// Fields:
+
+	private String title;		// Title of this DVD
+	private String rating;		// Rating of this DVD
+	private int runningTime;	// Running time of this DVD in minutes
+
+	public DVD(String dvdTitle, String dvdRating, int dvdRunningTime) 
+	{
+
+
+	
+	}
+	
+	public String getTitle() 
+	{
+
+
+
+		return null;	// STUB: Remove this line.
+	}
+	
+	public String getRating() 
+	{
+
+
+
+		return null;	// STUB: Remove this line.
+	}
+	
+	public int getRunningTime() 
+	{
+
+
+
+		return 0;	// STUB: Remove this line.
+	}
+
+	public void setTitle(String newTitle) {
+
+
+
+
+
+	}
+
+	public void setRating(String newRating) {
+
+
+
+
+
+	}
+
+	public void setRunningTime(int newRunningTime) {
+
+
+
+
+
+	}
+
+	public String toString() {
+
+
+
+		return null;	// STUB: Remove this line.
+	}
+	
+	
+}
---
+++ DVDCollection.java
@@ -0,0 +1,118 @@
+import java.io.*;
+
+public class DVDCollection {
+
+	// Data fields
+	
+	/** The current number of DVDs in the array */
+	private int numdvds;
+	
+	/** The array to contain the DVDs */
+	private DVD[] dvdArray;
+	
+	/** The name of the data file that contains dvd data */
+	private String sourceName;
+	
+	/** Boolean flag to indicate whether the DVD collection was
+	    modified since it was last saved. */
+	private boolean modified;
+	
+	/**
+	 *  Constructs an empty directory as an array
+	 *  with an initial capacity of 7. When we try to
+	 *  insert into a full array, we will double the size of
+	 *  the array first.
+	 */
+	public DVDCollection() {
+		numdvds = 0;
+		dvdArray = new DVD[7];
+	}
+	
+	public String toString() {
+		// Return a string containing all the DVDs in the
+		// order they are stored in the array along with
+		// the values for numdvds and the length of the array.
+		// See homework instructions for proper format.
+
+		
+
+
+
+		return null;	// STUB: Remove this line.
+	}
+
+	public void addOrModifyDVD(String title, String rating, String runningTime) {
+		// NOTE: Be careful. Running time is a string here
+		// since the user might enter non-digits when prompted.
+		// If the array is full and a new DVD needs to be added,
+		// double the size of the array first.
+
+
+
+
+
+
+	
+	}
+	
+	public void removeDVD(String title) {
+		
+
+
+
+
+
+
+
+	}
+	
+	public String getDVDsByRating(String rating) {
+
+
+
+
+
+
+		return null;	// STUB: Remove this line.
+
+	}
+
+	public int getTotalRunningTime() {
+
+
+
+
+
+
+
+		return 0;	// STUB: Remove this line.
+
+	}
+
+	
+	public void loadData(String filename) {
+
+
+
+
+
+
+		
+	}
+	
+	public void save() {
+
+
+
+
+
+
+
+	}
+
+	// Additional private helper methods go here:
+
+
+
+	
+}
