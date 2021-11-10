aea222f475419d0fd1a6392840becb3b8aba9efa
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-11-09 16:18:17 -0800

    Added documentation

--- DVDUserInterface.java
+++ DVDUserInterface.java
@@ -1,6 +1,8 @@
-
+/**
+ * Interface class for the user interface of the DVD Manager.
+ * As an interface class, the user interface can be implemented
+ * as a GUI, CLI, etc.
+ */
 public interface DVDUserInterface {
-
 	void processCommands();
-	
 }
