06099bae7378a6396fe16b614765b39025e0e3d1
Nickolas Alcazar <nickolasalcazar@gmail.com>
2021-11-09 16:02:47 -0800

    Reformatted code

--- DVDConsoleUI.java
+++ DVDConsoleUI.java
@@ -2,12 +2,9 @@ import java.util.*;
 
 /**
  *  This class is an implementation of DVDUserInterface
- *  that uses the console to display the menu of command choices
+ *  that uses the console to display the menu of command choices.
  */
-
-
 public class DVDConsoleUI implements DVDUserInterface {
-	
 	private DVDCollection dvdlist;
 	private Scanner scan;
 	
@@ -16,21 +13,18 @@ public class DVDConsoleUI implements DVDUserInterface {
 		scan = new Scanner(System.in);
 	}
 	
-	public void processCommands()
-	{
+	public void processCommands() {
 		 String[] commands = {"Add/Modify DVD",
 	 			  "Remove DVD",
 	 			  "Get DVDs By Rating",
 	 			  "Get Total Running Time",
 	 			  "Save and Exit"};
-
 		 int choice;
 		 
 		 do {
 			 for (int i = 0; i < commands.length; i++) {
 				 System.out.println("Select " + i + ": " + commands[i]);
-			 }
-			 try {
+			 } try {
 				 choice = scan.nextInt();
 				 scan.nextLine();
 				 switch (choice) {
@@ -39,58 +33,48 @@ public class DVDConsoleUI implements DVDUserInterface {
 				 	case 2: doGetDVDsByRating(); break;
 				 	case 3: doGetTotalRunningTime(); break;
 				 	case 4: doSave(); break;
-				 	default:  System.out.println("INVALID CHOICE - TRY AGAIN");
+				 	default: System.out.println("INVALID CHOICE - TRY AGAIN");
 				 }
 			 } catch (InputMismatchException e) {
-				 System.out.println("INVALID CHOICE - TRY AGAIN");
-				 scan.nextLine();
-				 choice = -1;
+				System.out.println("INVALID CHOICE - TRY AGAIN");
+				scan.nextLine();
+				choice = -1;
 			 }	 
 		 } while (choice != commands.length-1);
 	}
 
 	private void doAddOrModifyDVD() {
-
 		// Request the title
 		System.out.println("Enter title");
 		String title = scan.nextLine();
-		if (title.equals("")) {
-			return;		// input was cancelled
-		}
+		if (title.equals("")) return;		// input was cancelled
 		title = title.toUpperCase();
 		
 		// Request the rating
 		System.out.println("Enter rating");
 		String rating = scan.nextLine();
-		if (rating.equals("")) {
-			return;		// input was cancelled
-		}
+		if (rating.equals("")) return;		// input was cancelled
 		rating = rating.toUpperCase();
 		
 		// Request the running time
 		System.out.println("Enter running time");
 		String time = scan.nextLine();	// NOTE: time read in as a String!
-		if (time.equals("")) {
-			return;		// input was cancelled
-		}
+		if (time.equals("")) return;	// input was cancelled
 
 		// Add or modify the DVD (assuming the rating and time are valid)
 		dvdlist.addOrModifyDVD(title, rating, time);
 		
 		// Display current collection to the console for debugging
-		System.out.println("Adding/Modifying: " + title + "," + rating + "," + time);
+		System.out.println("Adding/Modifying: " 
+			+ title + "," + rating + "," + time);
 		System.out.println(dvdlist);
-		
 	}
 	
 	private void doRemoveDVD() {
-
 		// Request the title
 		System.out.println("Enter title");
 		String title = scan.nextLine();
-		if (title.equals("")) {
-			return;		// dialog was cancelled
-		}
+		if (title.equals("")) return;		// dialog was cancelled
 		title = title.toUpperCase();
 		
 		// Remove the matching DVD if found
@@ -99,36 +83,25 @@ public class DVDConsoleUI implements DVDUserInterface {
 		// Display current collection to the console for debugging
 		System.out.println("Removing: " + title);
 		System.out.println(dvdlist);
-			
 	}
 
-
 	private void doGetDVDsByRating() {
-
 		// Request the rating
 		System.out.println("Enter rating");
 		String rating = scan.nextLine();
-		if (rating.equals("")) {
-			return;		// dialog was cancelled
-		}
+		if (rating.equals("")) return;		// dialog was cancelled
 		rating = rating.toUpperCase();
 		
 		String results = dvdlist.getDVDsByRating(rating);
 		System.out.println("DVDs with rating " + rating);
 		System.out.println(results);		
-
 	}
 	
 	private void doGetTotalRunningTime() {
-
 		int total = dvdlist.getTotalRunningTime();
 		System.out.println("Total Running Time of DVDs: ");
 		System.out.println(total);
-
 	}
 	
-	private void doSave() {
-		dvdlist.save();
-	}	
-	
+	private void doSave() { dvdlist.save(); }	
 }
